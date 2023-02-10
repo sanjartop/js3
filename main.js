b467100c158e1804803001df51ac56d45c45ab8a
smailkusnanjar97 <smailkulovsanjar97@gmail.com>
2023-02-10 14:47:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+1
+//  function js3(a,b){
+//    if (a<b ) 
+//    alert('Второе число больше первого!');
+//   else if (a>b)
+//   alert(' первое число больше второгоого!');
+//  }
+//  js3(4,3)
+
+2
+// function TwoMass(arr1,arr2){}
+//     let arr1 = prompt ('Введитее первое слово')
+//     let arr2 = prompt ('Введитее второе слово')
+
+// if (arr1 > arr2){
+//     alert(arr1 + ' ' + 'больше чем' + ' ' + arr2)
+// }else if(arr1 < arr2){
+//     alert(arr1 + ' ' + 'меньше чем' + ' ' + arr2)
+// }else{
+//     alert('Они равны')
+// }
+
+3
+// function CountChar (){
+//     let a = prompt('введите слово  ')
+//     alert ('введенное слово имеет : ' + a.length + ' кодовых значений')
+// }
+//   CountChar()
+
+4
+// +DOP DZ
+// function killer (a,b,c){
+//  return (a + b + c );
+ 
+// }
+// let some_test = killer (1,2,3)
+// console.log(some_test)
